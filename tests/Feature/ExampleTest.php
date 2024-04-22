a051f6818f99fc1b6c98a61fa9eddf80b3ca809f
Your Name <you@example.com>
2024-04-22 21:34:56 +0500

    feat: laravel test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,19 +2,24 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Enums\Users\Role;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions;
 
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->assertDatabaseHas('users', [
+            'name'     => 'test',
+            'email'    => 'user1@insy.com',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
